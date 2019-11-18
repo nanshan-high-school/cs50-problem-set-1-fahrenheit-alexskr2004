439977066ab1bc6b40277c5ac00aec315ff7ac82
alexskr2004 <54940014+alexskr2004@users.noreply.github.com>
2019-11-18 22:50:22 +0800

    Create 270-19莊至翰Fahrenheit.cpp
    有兩個

---
+++ 270-19莊至翰Fahrenheit.cpp
@@ -0,0 +1,21 @@
+//第一個
+#include <iostream>
+using namespace std ;
+int main() {
+  float C = 0 ;
+    cout << "C=" ;
+    cin >> C ;
+  float F= ((C*9)/5)+32 ;
+   cout << "F="<<F  ;
+}
+
+//第二個
+#include <iostream>
+using namespace std ;
+int main() {
+  float F = 0 ;
+    cout << "F=" ;
+    cin >> F ;
+  float C= ((F-32)*5)/9 ;
+   cout << "C=" << C  ;
+}
